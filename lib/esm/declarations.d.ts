bd16bb3efe3ff3cd8313c93649fa33cb3a14ea5d
giles tetteh <giles.tetteh@bonescreen.de>
2025-11-01 14:04:32 +0100

    updated declaration file

--- lib/esm/declarations.d.ts
+++ lib/esm/declarations.d.ts
@@ -1,5 +1,9 @@
 import type { Application as FeathersApplication } from '@feathersjs/feathers';
-export type Application = FeathersApplication;
+export interface PatchedApplication extends FeathersApplication {
+    on(event: string, listener: (...args: any[]) => void): this;
+    once(event: string, listener: (...args: any[]) => void): this;
+}
+export type Application = PatchedApplication;
 declare module '@feathersjs/feathers' {
     interface ServiceAddons {
         findOne(params: any): Promise<any>;
--- src/declarations.ts
+++ src/declarations.ts
@@ -1,7 +1,13 @@
 // For more information about this file see https://dove.feathersjs.com/guides/cli/typescript.html
 import type { Application as FeathersApplication } from '@feathersjs/feathers'
 
-export type Application = FeathersApplication
+// Extend Application to include EventEmitter methods
+export interface PatchedApplication extends FeathersApplication {
+  on(event: string, listener: (...args: any[]) => void): this;
+  once(event: string, listener: (...args: any[]) => void): this;
+}
+
+export type Application = PatchedApplication
 
 // The context for hook functions - can be typed with a service class
 declare module '@feathersjs/feathers' {
