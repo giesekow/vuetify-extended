0cd7e3a0d321b04714a69f5b8897cf8e8d70091b
giles tetteh <giles.tetteh@bonescreen.de>
2025-11-01 14:09:20 +0100

    updated declaration file

--- lib/esm/declarations.d.ts
+++ lib/esm/declarations.d.ts
@@ -1,7 +1,17 @@
-import type { Application as FeathersApplication } from '@feathersjs/feathers';
+import type { Application as FeathersApplication, Service as FeathersService, ServiceAddons as FeathersServiceAddons } from '@feathersjs/feathers';
+/**
+ * Extend the Service interface to include EventEmitter methods
+ */
+export interface Service<T = any> extends FeathersService<T> {
+    on(event: string, listener: (...args: any[]) => void): this;
+    once(event: string, listener: (...args: any[]) => void): this;
+    emit(event: string, ...args: any[]): boolean;
+}
 export interface PatchedApplication extends FeathersApplication {
     on(event: string, listener: (...args: any[]) => void): this;
     once(event: string, listener: (...args: any[]) => void): this;
+    emit(event: string, ...args: any[]): boolean;
+    service<T = any>(path: string): Service<T> & FeathersServiceAddons;
 }
 export type Application = PatchedApplication;
 declare module '@feathersjs/feathers' {
--- src/declarations.ts
+++ src/declarations.ts
@@ -1,10 +1,23 @@
 // For more information about this file see https://dove.feathersjs.com/guides/cli/typescript.html
-import type { Application as FeathersApplication } from '@feathersjs/feathers'
+import type { Application as FeathersApplication, Service as FeathersService, ServiceAddons as FeathersServiceAddons } from '@feathersjs/feathers'
+
+/**
+ * Extend the Service interface to include EventEmitter methods
+ */
+export interface Service<T = any> extends FeathersService<T> {
+  on(event: string, listener: (...args: any[]) => void): this;
+  once(event: string, listener: (...args: any[]) => void): this;
+  emit(event: string, ...args: any[]): boolean;
+}
 
 // Extend Application to include EventEmitter methods
 export interface PatchedApplication extends FeathersApplication {
   on(event: string, listener: (...args: any[]) => void): this;
   once(event: string, listener: (...args: any[]) => void): this;
+  emit(event: string, ...args: any[]): boolean;
+
+  // Override service() to return extended Service type
+  service<T = any>(path: string): Service<T> & FeathersServiceAddons;
 }
 
 export type Application = PatchedApplication
